a39c6d220d4c6b162a227d03c880a7fa0eabf617
ArthurWang <wangyn@qeebike.com>
2019-01-18 13:44:18 +0800

    Update

--- app/src/main/java/com/example/RxThread/MainActivity.java
+++ app/src/main/java/com/example/RxThread/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.RxThread;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
@@ -8,6 +9,8 @@ import android.widget.TextView;
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
@@ -62,6 +65,42 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    private Observer<String> observerString = new Observer<String>() {
+        Disposable disposable;
+
+        @Override
+        public void onSubscribe(Disposable d) {
+            Log.e("wyn", "onSubscribe");
+
+            Log.e("wyn", "onSubscribe thread is " + Thread.currentThread().getName());
+
+            disposable = d;
+        }
+
+        @Override
+        public void onNext(String s) {
+            Log.d("wyn", "onNext is " + s);
+
+            Log.e("wyn", "onNext thread is " + Thread.currentThread().getName());
+
+            tvContent.setText(s);
+        }
+
+        @Override
+        public void onError(Throwable e) {
+            Log.e("wyn", "onError");
+
+            Log.e("wyn", "onError thread is " + Thread.currentThread().getName());
+        }
+
+        @Override
+        public void onComplete() {
+            Log.e("wyn", "onComplete");
+
+            Log.e("wyn", "onComplete thread is " + Thread.currentThread().getName());
+        }
+    };
+
 
 
 
@@ -86,11 +125,37 @@ public class MainActivity extends AppCompatActivity {
 
 //        testCreate();
 
-//        testInterval();
+        testInterval();
 
 
-        testTimer();
+//        testTimer();
+
+//        new Thread() {
+//            @Override
+//            public void run() {
+//                super.run();
+//
+//                testTimer();
+//            }
+//        }.start();
+
+//        Log.e("wyn", "111");
+//        postDelay();
+
+        // 在子线程中执行会崩溃 Can't create handler inside thread that has not called Looper.prepare()
+//        Log.e("wyn", "222");
+//        new Thread() {
+//            @Override
+//            public void run() {
+//                super.run();
+//
+//                postDelay();
+//            }
+//        }.start();
+
     }
+
+
 
 
 
@@ -99,50 +164,63 @@ public class MainActivity extends AppCompatActivity {
 //            .subscribe(this.observer);
 //    }
 //
-//    private void testCreate() {
-//        Observable.create(new ObservableOnSubscribe<String>() {
-//            @Override
-//            public void subscribe(ObservableEmitter<String> emitter) throws Exception {
-//                Log.e("wyn", "ObservableEmitter");
-//
-//                Log.e("wyn", "ObservableEmitter thread is " + Thread.currentThread().getName());
-//
-//
-//                long a = 1;
-//                for (int i = 0; i < 1000000000; i++) {
-//                    a = a + (a + 1);
-//                }
-//
-//                Log.e("wyn", "a is " + a);
-//
-//                emitter.onNext("wang" + a);
-//                emitter.onNext("yinan");
-//
-//                emitter.onComplete();
-//            }
-//        }).subscribeOn(Schedulers.io())
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .subscribe(this.observer);
-//    }
+    private void testCreate() {
+        Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(ObservableEmitter<String> emitter) throws Exception {
+                Log.e("wyn", "ObservableEmitter");
+
+                Log.e("wyn", "ObservableEmitter thread is " + Thread.currentThread().getName());
+
+
+                long a = 1;
+                for (int i = 0; i < 1000000000; i++) {
+                    a = a + (a + 1);
+                }
+
+                Log.e("wyn", "a is " + a);
+
+                emitter.onNext("wang" + a);
+                emitter.onNext("yinan");
+
+                emitter.onComplete();
+            }
+        }).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(this.observerString);
+    }
 
     private void testInterval() {
-        Log.e("wyn", "111");
+//        Log.e("wyn", "111");
         Observable.interval(3, TimeUnit.SECONDS)
-                .subscribeOn(Schedulers.io())
+                .subscribeOn(Schedulers.computation())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(this.observer);
 
-        Log.e("wyn", "222");
+//        Log.e("wyn", "222");
     }
 
     private void testTimer() {
-        Log.e("wyn", "333");
+//        Log.e("wyn", "333");
 
         Observable.timer(3, TimeUnit.SECONDS)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(this.observer);
 
-        Log.e("wyn", "444");
+//        Log.e("wyn", "444");
     }
+
+
+    private void postDelay() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Log.e("wyn", "postDelayed");
+
+                Log.e("wyn", "postDelayed thread is " + Thread.currentThread().getName());
+            }
+        }, 3000);
+    }
+
 }
